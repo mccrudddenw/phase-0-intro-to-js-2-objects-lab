3ec87c5e5c4b2b12b2d4194fa5a596940825c29e
mccruddenw <wyatt.mccrudden@gmail.com>
2021-03-10 15:44:33 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution in this file!
+
+
+const employee = {
+    name: 'wyatt',
+    streetAddress: '6049 Rose Loop NE'
+  
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+     
+    const newObj = {...employee};
+
+    newObj[key] = value;  
+
+
+    return newObj;
+
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    
+    obj[key] = value;
+
+    return obj;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+
+    const newObj = {...employee};
+
+    delete newObj[key];
+
+    return newObj;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key];
+
+    return employee;
+
+}
